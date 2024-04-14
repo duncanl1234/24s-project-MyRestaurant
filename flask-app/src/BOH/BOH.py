4978fb484f01b4726f1794517df77851a889b699
msilveri <163439430+msilveri@users.noreply.github.com>
2024-04-14 14:47:31 -0400

    Update BOH.py
    delete copies

--- flask-app/src/BOH/BOH.py
+++ flask-app/src/BOH/BOH.py
@@ -1,6 +1,5 @@
 ########################################################
 # Sample orders blueprint of endpoints
-# Remove this file if you are not using it in your project
 ########################################################
 from flask import Blueprint, request, jsonify, make_response, current_app
 import json
